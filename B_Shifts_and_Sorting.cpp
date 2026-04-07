ed1e23c6c3a9d99f705ade13b5a1bcb157c87cbb
Abhisar Tyagi <abhisartyagi24@gmail.com>
2026-04-07 21:48:44 +0530

    Revised code with correction

--- B_Shifts_and_Sorting.cpp
+++ B_Shifts_and_Sorting.cpp
@@ -28,27 +28,22 @@ int main(){
         cin>>s;
         vll cnt1,cnt0;
         for(ll i=0;i<s.size();i++){
-            if(s[i]=='0')cnt1.pb(i);
-            else cnt0.pb(i);
+            if(s[i]=='0')cnt0.pb(i);
+            else cnt1.pb(i);
         }
-        // if(cnt1.size()==0 || cnt0.size()==0) {cout<<0<<endl;continue;}
-        // else{
-        //     ll ans=0;
-        //     for(ll i:cnt0){
-        //         if(i<cnt1[0])continue;
-        //         else{
-        //             ans+= (i-cnt1[0]+1);
-        //             cnt1[0]++;
-
-        //         }
-        //     }
-        //     cout<<ans<<endl;
-        // }
-        for(ll i:cnt0)cout<<i<<" ";
-        cout<<endl;
-        for(ll i:cnt1)cout<<i<<" ";
-        cout<<endl;
-        
+        if(cnt1.size()==0 || cnt0.size()==0) {cout<<0<<endl;continue;}
+        else{
+            ll ans=0;
+            ll temp=cnt1[0];
+            for(ll i=0;i<cnt0.size();i++){
+                if(cnt0[i]<temp)continue;
+                else{
+                    ans+=cnt0[i]-temp+1;
+                    temp++;
+                }
+            }
+            cout<<ans<<endl;
+        }
 
     }
     return 0;
